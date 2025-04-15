3849c0a9fa9a12753007af3d6b7ad51606423ff5
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-04-15 16:27:23 +0530

    feat: add navbar component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,23 +1,50 @@
-import React from 'react'
-import Image from 'next/image'
-import logo from '../src/assets/Logo.png'
-import {GrFacebookOption, GrTwitter, GrLinkedinOption} from 'react-icons/gr'
+import React from "react";
+import Image from "next/image";
+import logo from "../src/assets/olivra.png";
+import { GrFacebookOption, GrTwitter, GrLinkedinOption } from "react-icons/gr";
 
 const Footer = () => {
   return (
     <footer>
-      <div className='footer'>
-        <div className='logo'>
-          <Image src={logo} width={180} height={30} alt='logo' />
-          <p>Small, artisan label that offers a thoughtfully curated collection of high quality everyday essentials made.</p>
-          <div className='icon-container'>
-            <div><GrTwitter size={20} /></div>
-            <div><GrFacebookOption size={20} /></div>
-            <div><GrLinkedinOption size={20} /></div>
+      <div className="footer">
+        <div className="logo">
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              textDecoration: "none",
+              color: "inherit",
+            }}
+          >
+            <Image src={logo} width={100} height={100} alt="logo" />
+            <span
+              style={{
+                fontSize: "2.5rem",
+                fontWeight: 700,
+                color: "#8ea092",
+              }}
+            >
+              Olivra
+            </span>
+          </div>
+          <p>
+            Small, artisan label that offers a thoughtfully curated collection
+            of high quality everyday essentials made.
+          </p>
+          <div className="icon-container">
+            <div>
+              <GrTwitter size={20} />
+            </div>
+            <div>
+              <GrFacebookOption size={20} />
+            </div>
+            <div>
+              <GrLinkedinOption size={20} />
+            </div>
           </div>
         </div>
 
-        <div className='footer-links'>
+        <div className="footer-links">
           <h3>Company</h3>
           <ul>
             <li>About</li>
@@ -28,7 +55,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className='footer-links'>
+        <div className="footer-links">
           <h3>Support</h3>
           <ul>
             <li>Support Carrer</li>
@@ -37,7 +64,7 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className='footer-links'>
+        <div className="footer-links">
           <h3>Contact</h3>
           <ul>
             <li>Whatsapp</li>
@@ -46,13 +73,11 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='copyright'>
-        <p>Copyright © 2022 Dine Market</p>
-        <p>Design by. <span>Weird Design Studio</span></p>
-        <p>Code by. <span>shabrina12 on github</span></p>
+      <div className="copyright">
+        <p>Copyright © 2025 Olivra</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,78 +1,149 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
-import {CiSearch} from 'react-icons/ci'
-import {CgShoppingCart} from 'react-icons/cg'
-import logo from '../src/assets/Logo.png'
-import Link from 'next/link'
-import {RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import { useStateContext } from '../context/StateContext';
+"use client";
 
-const Navbar = ({Searchproducts}) => {
-  const {showCart, setShowCart, totalQty} = useStateContext();
+import React, { useState } from "react";
+import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
+import { CgShoppingCart } from "react-icons/cg";
+import logo from "../src/assets/olivra.png";
+import Link from "next/link";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { useStateContext } from "../context/StateContext";
+import { usePathname } from "next/navigation";
+
+const Navbar = ({ Searchproducts }) => {
+  const { showCart, setShowCart, totalQty } = useStateContext();
   const [toggleMenu, setToggleMenu] = useState(false);
+  const pathname = usePathname();
   // const [searchTerm, setSearchTerm] = useState('')
+
+  const navLinks = [
+    { href: "/female", label: "Female" },
+    { href: "/male", label: "Male" },
+    { href: "/kids", label: "Kids" },
+    { href: "/products", label: "All Products" },
+  ];
 
   return (
     <nav>
-      <Link href='/'>
-        <Image src={logo} width={140} height={25} alt='logo' />
+      <Link href="/" passHref>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            textDecoration: "none",
+            color: "inherit",
+          }}
+        >
+          <Image src={logo} width={100} height={100} alt="logo" />
+          <span
+            style={{
+              fontSize: "2.5rem",
+              fontWeight: 700,
+              color: "#8ea092",
+            }}
+          >
+            Olivra
+          </span>
+        </div>
       </Link>
-      <ul className='nav-links'>
-        <Link href='/female'><li>Female</li></Link>
-        <Link href='/male'><li>Male</li></Link>
-        <Link href='/kids'><li>Kids</li></Link>
-        <Link href='/products'><li>All Products</li></Link>
+      <ul className="nav-links">
+        {navLinks.map((link) => (
+          <li key={link.href}>
+            <Link
+              href={link.href}
+              className={`nav-link ${
+                pathname === link.href ? "active-link" : ""
+              }`}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
       </ul>
 
-        <div className='search-bar'>
-          <CiSearch />
-          <input 
-            type='text' 
-            placeholder='What you looking for'/>
-        </div>
-        {/* onChange={(event) => {
+      <div className="search-bar">
+        <CiSearch />
+        <input type="text" placeholder="What you looking for" />
+      </div>
+      {/* onChange={(event) => {
               setSearchTerm(event.target.value);
           }} */}
-
-      {showCart ?
-      <Link href='/cart'>
-        <button className='cart' onClick={() => setShowCart(false)}>   
+      {showCart ? (
+        <Link href="/cart">
+          <button className="cart" onClick={() => setShowCart(false)}>
+            <CgShoppingCart size={22} />
+            <span className="cart-item-qty">{totalQty}</span>
+          </button>
+        </Link>
+      ) : (
+        <button className="cart" onClick={() => setShowCart(true)}>
           <CgShoppingCart size={22} />
-          <span className='cart-item-qty'>{totalQty}</span> 
+          <span className="cart-item-qty">{totalQty}</span>
         </button>
-      </Link> 
-      : 
-      <button className='cart' onClick={() => setShowCart(true)}> 
-        <CgShoppingCart size={22} />
-        <span className='cart-item-qty'>{totalQty}</span>
-      </button> 
-      }
-
-      <div className='navbar-smallscreen'>
-        <RiMenu3Line color='black' fontSize={27} onClick={() => setToggleMenu(true)} />
+      )}
+      <div className="navbar-smallscreen">
+        <RiMenu3Line
+          color="black"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
         {toggleMenu && (
-          <div className='navbar-smallscreen_overlay'>
-            <Link href='/'>
-              <Image className='logo-small' src={logo} width={140} height={25} alt='logo' />
+          <div className="navbar-smallscreen_overlay">
+            <Link href="/" passHref>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  textDecoration: "none",
+                  color: "inherit",
+                }}
+              >
+                <Image src={logo} width={100} height={100} alt="logo" />
+                <span
+                  style={{
+                    fontSize: "2.5rem",
+                    fontWeight: 700,
+                    color: "Green",
+                  }}
+                >
+                  Olivra
+                </span>
+              </div>
             </Link>
-            <RiCloseLine  color='black' fontSize={27} className='close_icon' onClick={() => setToggleMenu(false)} />
-            <ul className='navbar-smallscreen_links'>
-              <Link href='/cart'>
-                  <button className='cart-small-screen' onClick={() => setShowCart(false)}>   
-                    <CgShoppingCart size={22} />
-                    <span className='cart-item-qty'>{totalQty}</span> 
-                  </button>
-              </Link> 
-              <Link href='/female'><li>Female</li></Link>
-              <Link href='/male'><li>Male</li></Link>
-              <Link href='/kids'><li>Kids</li></Link>
-              <Link href='/products'><li>All Products</li></Link>
+            <RiCloseLine
+              color="black"
+              fontSize={27}
+              className="close_icon"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="navbar-smallscreen_links">
+              <Link href="/cart">
+                <button
+                  className="cart-small-screen"
+                  onClick={() => setShowCart(false)}
+                >
+                  <CgShoppingCart size={22} />
+                  <span className="cart-item-qty">{totalQty}</span>
+                </button>
+              </Link>
+              <Link href="/female">
+                <li>Female</li>
+              </Link>
+              <Link href="/male">
+                <li>Male</li>
+              </Link>
+              <Link href="/kids">
+                <li>Kids</li>
+              </Link>
+              <Link href="/products">
+                <li>All Products</li>
+              </Link>
             </ul>
           </div>
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
