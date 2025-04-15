f16ed5b8a38feaac29c2f46831d0c1d7d5759da7
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-04-15 11:58:10 +0530

    feat: update navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,78 +1,110 @@
-import React, { useState } from 'react'
-import Image from 'next/image'
-import {CiSearch} from 'react-icons/ci'
-import {CgShoppingCart} from 'react-icons/cg'
-import logo from '../src/assets/Logo.png'
-import Link from 'next/link'
-import {RiMenu3Line, RiCloseLine } from 'react-icons/ri';
-import { useStateContext } from '../context/StateContext';
+import React, { useState } from "react";
+import Image from "next/image";
+import { CiSearch } from "react-icons/ci";
+import { CgShoppingCart } from "react-icons/cg";
+import logo from "../src/assets/Logo.png";
+import Link from "next/link";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import { useStateContext } from "../context/StateContext";
 
-const Navbar = ({Searchproducts}) => {
-  const {showCart, setShowCart, totalQty} = useStateContext();
+const Navbar = ({ Searchproducts }) => {
+  const { showCart, setShowCart, totalQty } = useStateContext();
   const [toggleMenu, setToggleMenu] = useState(false);
   // const [searchTerm, setSearchTerm] = useState('')
 
   return (
     <nav>
-      <Link href='/'>
-        <Image src={logo} width={140} height={25} alt='logo' />
+      <Link href="/">
+        <Image src={logo} width={140} height={25} alt="logo" />
       </Link>
-      <ul className='nav-links'>
-        <Link href='/female'><li>Female</li></Link>
-        <Link href='/male'><li>Male</li></Link>
-        <Link href='/kids'><li>Kids</li></Link>
-        <Link href='/products'><li>All Products</li></Link>
+      <ul className="nav-links">
+        <Link href="/female">
+          <li>Female</li>
+        </Link>
+        <Link href="/male">
+          <li>Male</li>
+        </Link>
+        <Link href="/kids">
+          <li>Kids</li>
+        </Link>
+        <Link href="/products">
+          <li>All Products</li>
+        </Link>
       </ul>
 
-        <div className='search-bar'>
-          <CiSearch />
-          <input 
-            type='text' 
-            placeholder='What you looking for'/>
-        </div>
-        {/* onChange={(event) => {
-              setSearchTerm(event.target.value);
+      <div className="search-bar">
+        <CiSearch />
+        <input type="text" placeholder="What you looking for" />
+      </div>
+      {/* onChange={(event) => {
+              setSearchTerm(event.target.value); //new
           }} */}
 
-      {showCart ?
-      <Link href='/cart'>
-        <button className='cart' onClick={() => setShowCart(false)}>   
+      {showCart ? (
+        <Link href="/cart">
+          <button className="cart" onClick={() => setShowCart(false)}>
+            <CgShoppingCart size={22} />
+            <span className="cart-item-qty">{totalQty}</span>
+          </button>
+        </Link>
+      ) : (
+        <button className="cart" onClick={() => setShowCart(true)}>
           <CgShoppingCart size={22} />
-          <span className='cart-item-qty'>{totalQty}</span> 
+          <span className="cart-item-qty">{totalQty}</span>
         </button>
-      </Link> 
-      : 
-      <button className='cart' onClick={() => setShowCart(true)}> 
-        <CgShoppingCart size={22} />
-        <span className='cart-item-qty'>{totalQty}</span>
-      </button> 
-      }
+      )}
 
-      <div className='navbar-smallscreen'>
-        <RiMenu3Line color='black' fontSize={27} onClick={() => setToggleMenu(true)} />
+      <div className="navbar-smallscreen">
+        <RiMenu3Line
+          color="black"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
         {toggleMenu && (
-          <div className='navbar-smallscreen_overlay'>
-            <Link href='/'>
-              <Image className='logo-small' src={logo} width={140} height={25} alt='logo' />
+          <div className="navbar-smallscreen_overlay">
+            <Link href="/">
+              <Image
+                className="logo-small"
+                src={logo}
+                width={140}
+                height={25}
+                alt="logo"
+              />
             </Link>
-            <RiCloseLine  color='black' fontSize={27} className='close_icon' onClick={() => setToggleMenu(false)} />
-            <ul className='navbar-smallscreen_links'>
-              <Link href='/cart'>
-                  <button className='cart-small-screen' onClick={() => setShowCart(false)}>   
-                    <CgShoppingCart size={22} />
-                    <span className='cart-item-qty'>{totalQty}</span> 
-                  </button>
-              </Link> 
-              <Link href='/female'><li>Female</li></Link>
-              <Link href='/male'><li>Male</li></Link>
-              <Link href='/kids'><li>Kids</li></Link>
-              <Link href='/products'><li>All Products</li></Link>
+            <RiCloseLine
+              color="black"
+              fontSize={27}
+              className="close_icon"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="navbar-smallscreen_links">
+              <Link href="/cart">
+                <button
+                  className="cart-small-screen"
+                  onClick={() => setShowCart(false)}
+                >
+                  <CgShoppingCart size={22} />
+                  <span className="cart-item-qty">{totalQty}</span>
+                </button>
+              </Link>
+              <Link href="/female">
+                <li>Female</li>
+              </Link>
+              <Link href="/male">
+                <li>Male</li>
+              </Link>
+              <Link href="/kids">
+                <li>Kids</li>
+              </Link>
+              <Link href="/products">
+                <li>All Products</li>
+              </Link>
             </ul>
           </div>
         )}
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
