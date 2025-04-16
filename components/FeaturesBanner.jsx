c194a4736da3618711a3d70a6bce42fee11abc63
Wandana Mddumage <wandanamuthumali99@gmail.com>
2025-04-16 09:13:47 +0530

    feat: add product details page

--- components/FeaturesBanner.jsx
+++ components/FeaturesBanner.jsx
@@ -1,20 +1,18 @@
-import React from 'react'
-import Image from 'next/image'
-import img from '../src/assets/feature.png'
-import Link from 'next/link'
+import React from "react";
+import Image from "next/image";
+import img from "../src/assets/I2.jpg";
+import Link from "next/link";
 
 const FeaturesBanner = () => {
   return (
-    <section className='features-section'>
-      <div className='title'>
-        <h1>Unique and Authentic Vintage Designer Jewellery</h1>
+    <section className="features-section">
+      <div className="title">
+        <h1>Original Designs. Lasting Impressions.</h1>
       </div>
 
-      <div className='content'>
-        <div className='left'>
-          <div className="feature-background">
-            Different from others
-          </div>
+      <div className="content">
+        <div className="left">
+          <div className="feature-background">Different from others</div>
           <div>
             <h3>Using Good Quality Materials</h3>
             <p>Lorem ipsum dolor sit amt, consectetur adipiscing elit.</p>
@@ -33,18 +31,25 @@ const FeaturesBanner = () => {
           </div>
         </div>
 
-        <div className='right'>
-          <Image src={img} width={300} height={350} alt='img' />
+        <div className="right">
+          <Image src={img} width={300} height={350} alt="img" />
           <div>
-            <p>This piece is ethically crafted in our small family-owned workshop in Peru with unmatched attention to detail and care. The Natural color is the actual natural color of the fiber, undyed and 100% traceable.</p>
-            <Link href={'/products'}>
-              <button className='btn' type='button'>See All Product</button>
+            <p>
+              This piece is ethically crafted in our small family-owned workshop
+              in Peru with unmatched attention to detail and care. The Natural
+              color is the actual natural color of the fiber, undyed and 100%
+              traceable.
+            </p>
+            <Link href={"/products"}>
+              <button className="btn" type="button">
+                See All Product
+              </button>
             </Link>
           </div>
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default FeaturesBanner
+export default FeaturesBanner;
